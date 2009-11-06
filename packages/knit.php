30e2852c60ed68621341765160755b31f805e4c3
Michael Mathews <michael.mathews@bbc.co.uk>
2009-11-06 15:20:36 +0000

    Modified knit.php so that it will work with js or css packages.

--- packages/knit.php
+++ packages/knit.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+	Given a query like: '?package=name.js' or '?package=name.css'
+	will dynamically pull together all the files to make that package.
+	
+	Uses the packages.xml file to decide which files belong to which packages.
+*/
+
 
 /**** configure ****/
 
@@ -21,10 +28,12 @@ if ( empty($args['package']) ) {
  	throw new Exception('Required GET parameter "package" is empty.');
 }
 
-$js_files = getFiles($packages, $args['package'], 'js');
+list($package_name, $type) = split('\.', $args['package']);
 
-header('Content-Type: text/javascript');
-knitFiles($js_files, $conf['path_to_root']);
+$files = getFiles($packages, $package_name, $type);
+
+printHeader($type);
+knitFiles($files, $conf['path_to_root']);
 
 
 /**** functions ****/
@@ -35,7 +44,7 @@ function getSafeArgs($unsafe_args) {
 	$safe_args = array();
 	
 	if ( !empty($unsafe_args['package']) ) {
-		$safe_args['package'] = preg_replace('/[^a-zA-Z_0-9-]/', '', $unsafe_args['package']);
+		$safe_args['package'] = preg_replace('/[^a-zA-Z_0-9.-]/', '', $unsafe_args['package']);
 		if ($unsafe_args['package'] != $safe_args['package']) {
 			throw new Exception('Argument "package" ' . $unsafe_args['package'] . ' has unsafe characters and cannot be used.');
 		}
@@ -63,8 +72,8 @@ function getPackages($address) {
 
 /**
  */
-function getFiles($packages, $package, $type) {
-	$file_list = $packages->xpath("/packages/$package/$type");
+function getFiles($packages, $package_name, $type) {
+	@$file_list = $packages->xpath("/packages/$package_name/$type");
 	if ( empty($file_list) ) {
 		throw new Exception('No data is defined for the package with that name and type.');
 	}
@@ -75,6 +84,19 @@ function getFiles($packages, $package, $type) {
   		$files[] = $file;
   	}
   	return $files;
+}
+
+/**
+ */
+function printHeader($type) {
+	switch ($type) {
+		case 'js':
+			header('Content-Type: text/javascript');
+			break;
+		case 'css':
+			header('Content-Type: text/css');
+			break;
+	}
 }
 
 /**
