d30ff96b30552a35d3cb975da7a1cd322b154439
Michael Mathews <michael.mathews@bbc.co.uk>
2009-11-25 16:29:21 +0000

    Fix for missing Glow.complete() for commando glow.

--- src/header/core.js
+++ src/header/core.js
@@ -6,5 +6,5 @@
 
 if (typeof Glow == 'undefined') {
 	glow = (typeof glow == 'undefined')? {} : glow;
-	Glow = { provide: function(module) { module.builder(glow); } };
+	Glow = { provide: function(module) { module.builder(glow); }, complete: function(){} };
 }
