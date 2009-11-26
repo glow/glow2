e1de6dfd51c9968ce0373d23c934c46cb7fb1dae
Jake Archibald <jake.archibald@bbc.co.uk>
2009-11-26 16:26:41 +0000

    Events: Adding speed test

---
+++ test/speed/glow/events/glow-tests.js
@@ -0,0 +1,18 @@
+// First param is the library name, as defined in woosh.libs
+woosh.addTests('glow-170', {
+	'$preTest': function(prevTest, nextTest) {
+		if (!prevTest) {
+			window.testObj = {};
+			window.eventsFired = 0;
+		}
+	},
+	'Adding Custom Listeners': new woosh.Test(100000, function() {
+		glow.events.addListener(testObj, 'testEvent', function() {
+			eventsFired++;
+		});
+	}),
+	'Firing Custom Listeners': new woosh.Test(1, function() {
+		glow.events.fire(testObj, 'testEvent');
+		return eventsFired;
+	})
+});
