9063e371f2a2db7b8114f5c649189d30195a9a6a
Jake Archibald <jake.archibald@bbc.co.uk>
2009-11-26 16:54:39 +0000

    Events: Forgot to add js file to previous commit - oops

---
+++ test/speed/glow/events/glow2-tests.js
@@ -0,0 +1,18 @@
+// First param is the library name, as defined in woosh.libs
+woosh.addTests('glow2-src', {
+	'$preTest': function(prevTest, nextTest) {
+		if (!prevTest) {
+			window.testObj = new glow.events.Target();
+			window.eventsFired = 0;
+		}
+	},
+	'Adding Custom Listeners': new woosh.Test(100000, function() {
+		testObj.on('testEvent', function() {
+			eventsFired++;
+		});
+	}),
+	'Firing Custom Listeners': new woosh.Test(1, function() {
+		testObj.fire('testEvent');
+		return eventsFired;
+	})
+});
