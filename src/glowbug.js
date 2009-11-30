83f4ca0f860e5905da3b1654045923f97b515599
Michael Mathews <micmath@gmail.com>
2009-11-30 14:26:14 +0000

    Added support for Opera in glowbug.

--- src/glowbug.js
+++ src/glowbug.js
@@ -3,17 +3,26 @@ var glowbug = {
 		if (window.console && typeof window.console.log == 'function') {
 			console.log(msg);
 		}
+		else if (window.opera && opera.postError) {
+			opera.postError('[LOG] ' + msg);
+		}
 	}
 	,
 	warn: function(msg) {
 		if (window.console && typeof window.console.warn == 'function') {
 			console.warn(msg);
 		}
+		else if (window.opera && opera.postError) {
+			opera.postError('[WARN] ' + msg);
+		}
 	}
 	,
 	error: function(msg) {
 		if (window.console && typeof window.console.error == 'function') {
 			console.error(msg);
+		}
+		else if (window.opera && opera.postError) {
+			opera.postError('[ERROR] ' + msg);
 		}
 	}
 };
