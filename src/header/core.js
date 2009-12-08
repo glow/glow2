a3b0753e39db4a8bfe86cc6bf294b02dc74510ff
Michael Mathews <michael.mathews@bbc.co.uk>
2009-12-08 18:31:00 +0000

    Changes to build script to use YUI compressor and generate JsDoc.

--- src/header/core.js
+++ src/header/core.js
@@ -1,9 +1,3 @@
-/*
- * BBC Glow javaScript Library (c) 2009
- * Package: core
- * Revision: 1ac626v8222j332l2k2wscdaa163
- */
-
 if (typeof Glow == 'undefined') {
 	glow = (typeof glow == 'undefined')? {} : glow;
 	Glow = { provide: function(module) { module.builder(glow); }, complete: function(){} };
--- src/header/widgets.js
+++ src/header/widgets.js
@@ -1,9 +1,3 @@
-/*
- * BBC Glow javaScript Library (c) 2009
- * Package: widgets
- * Revision: 4t6334fs281ac6222j337fa16v82
- */
-
 if (typeof Glow == 'undefined') {
 	glow = (typeof glow == 'undefined')? {} : glow;
 	Glow = { provide: function(module) { module.builder(glow); }, complete: function(){} };
