776dc50b50370465edecbd60009682d4d9d048d3
Frances Berriman <frances.berriman@bbc.co.uk>
2010-01-07 11:22:54 +0000

    Added better glow1&2 speed tests

--- test/speed/glow/NodeList/traversal/glow-tests.js
+++ test/speed/glow/NodeList/traversal/glow-tests.js
@@ -8,18 +8,21 @@ woosh.addTests('glow-170', {
 		}
 	},
 	'NodeList#ancestors': new woosh.Test(1000, function() {
-		return glow.dom.get('#univnmsp').ancestors();
+		return glow.dom.get('#univnmsp').ancestors().length;
 	}),
 	'NodeList#parent': new woosh.Test(1000, function() {
-		return glow.dom.get('#univnmsp').parent();
+		return glow.dom.get('#univnmsp').parent().length;
+	}),
+	'NodeList#get': new woosh.Test(10000, function() {
+		return new glow.dom.get('#univnmsp').get().length;
 	}),
 	'NodeList#prev': new woosh.Test(10000, function() {
-		return glow.dom.get('#univnmsp').prev();
+		return glow.dom.get('#univnmsp').prev().length;
 	}),
-	'NodeList#next': new woosh.Test(1000, function() {
-		return glow.dom.create('#univnmsp').next();
+	'NodeList#next': new woosh.Test(10000, function() {
+		return glow.dom.get('#univnmsp').next().length;
 	}),
 	'NodeList#contains': new woosh.Test(10000, function() {
-		return glow.dom.create('univnmsp').iswithin('subtocunivnmsp');
+		return glow.dom.get('#univnmsp').isWithin('subtocunivnmsp');
 	})
 });
--- test/speed/glow/NodeList/traversal/glow2-tests.js
+++ test/speed/glow/NodeList/traversal/glow2-tests.js
@@ -1,27 +1,25 @@
 woosh.addTests('glow2-src', {
 	'$preTest': function(prevTest, nextTest) {
 		if (!prevTest) {
-			// initial setup
-			window.htmlForTest = document.getElementById('htmlForTest');
-			window.largeHtmlCollection = htmlForTest.getElementsByTagName('*');
+			
 		}
 	},
 	'NodeList#ancestors': new woosh.Test(1000, function() {
-		return new glow.NodeList('#univnmsp').ancestors();
+		return new glow.NodeList('#univnmsp').ancestors().length;
 	}),
 	'NodeList#parent': new woosh.Test(1000, function() {
-		return new glow.NodeList('#univnmsp').parent();
+		return new glow.NodeList('#univnmsp').parent().length;
 	}),
 	'NodeList#get': new woosh.Test(10000, function() {
-		return new glow.NodeList('#htmlForTest').get();
+		return new glow.NodeList('#htmlForTest').get().length;
 	}),
 	'NodeList#prev': new woosh.Test(10000, function() {
-		return new glow.NodeList('#univnmsp').prev();
+		return new glow.NodeList('#univnmsp').prev().length;
 	}),
 	'NodeList#next': new woosh.Test(10000, function() {
-		return new glow.NodeList('#univnmsp').next();
+		return new glow.NodeList('#univnmsp').next().length;
 	}),
-	'NodeList#contains': new woosh.Test(1000, function() {
+	'NodeList#contains': new woosh.Test(10000, function() {
 		return new glow.NodeList('subtocunivnmsp').contains('univnmsp');
 	})
 });
