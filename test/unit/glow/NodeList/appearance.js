5c89d3eacf1466edb8f97e3621ad3019d5f96cdb
Frances Berriman <frances.berriman@bbc.co.uk>
2010-01-18 11:09:28 +0000

    Adding speed test stubs for NodeList#appearance methods

---
+++ test/unit/glow/NodeList/appearance.js
@@ -0,0 +1,41 @@
+function byId(id) {
+	return document.getElementById(id);
+}
+
+module('glow.NodeList appearance');
+
+test('glow.NodeList#css', 0, function() {
+
+});
+
+test('glow.NodeList#height', 0, function() {
+
+});
+
+test('glow.NodeList#width', 0, function() {
+
+});
+
+test('glow.NodeList#scrollLeft', 0, function() {
+
+});
+
+test('glow.NodeList#scrollTop', 0, function() {
+
+});
+
+test('glow.NodeList#hide', 0, function() {
+		
+});
+
+test('glow.NodeList#show', 0, function() {
+	
+});
+
+test('glow.NodeList#offset', 0, function() {
+	
+});
+
+test('glow.NodeList#position', 0, function() {
+	
+});
