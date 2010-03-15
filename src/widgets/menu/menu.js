cab5d3f35a502fb219f2414579e4cc844643dcb6
Frances Berriman <frances.berriman@bbc.co.uk>
2010-03-15 10:59:46 +0000

    Menu API WIP

---
+++ src/widgets/menu/menu.js
@@ -0,0 +1,12 @@
+/**
+	@name glow.widgets.Menu
+	@class
+	@augments glow.widgets.Overlay
+	@description An overlay with default drop-down selection behaviour.
+	@param {Object} opts
+			Zero or more of the following as properties of an object:
+			@param {Number|String} [opts.width=400] Width of the menu
+				Default of 400px gives a content width of 360px in the default template
+			@param {Number|String} [opts.height=auto] Height of the menu
+				Default of auto, content larger than this will cause scrollable behaviour
+**/
