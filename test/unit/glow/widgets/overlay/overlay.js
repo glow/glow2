bc5679447e4f900161878362c66a9802c4a8e05a
Frances Berriman <frances.berriman@bbc.co.uk>
2010-03-16 21:24:36 +0000

    Overlay unit test stubs

---
+++ test/unit/glow/widgets/overlay/overlay.js
@@ -0,0 +1,31 @@
+module('glow.ui.Overlay');
+	
+      
+	test('Basic overlay creation', function() {			
+        expect(2);	
+				
+		var myOverlay = new glow.widgets.Overlay('<div>Hello world!</div>');
+							
+		ok( (myOverlay), 'myOverlay exists' );
+
+        ok(glow("#glow-ui-overlay-container").length == 1, 'A container element has been created')
+
+
+	});
+
+	test('Check inherited base methods', function() {			
+        expect(1);	
+				
+		var myOverlay = new glow.widgets.Overlay('<div>Hello world!</div>');
+							
+		ok( (myOverlay), 'myOverlay exists' );
+
+        myOverlay.disable();
+
+        myOverlay.enabled();
+
+        myOverlay.locale('pirate');
+
+      
+
+	});
