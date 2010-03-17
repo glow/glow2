f2d85d8acf73b9d68526cb21d132538b7c384337
Frances Berriman <frances.berriman@bbc.co.uk>
2010-03-17 17:18:50 +0000

    more focusmanager API

--- src/ui/FocusManager/FocusManager.js
+++ src/ui/FocusManager/FocusManager.js
@@ -2,17 +2,19 @@
 		@name glow.ui.FocusManager
 		@class
 		@augments glow.ui
-		@description A component for creating focussable widgets
+		@description A component for creating focussable widgets, such as a menu
 		@param {selector|NodeList|String} content
-			the element that contains the contents of the overlay. If this is
-			in the document it will be moved to document.body.
+			the element that contains the items to form the focussable object.
 		@param {object} opts
 			Zero or more of the following as properties of an object:
 			@param {Boolean} [opts.modality=1] The modality of the focussable object.
 			@param {String|NodeList|selector} [opts.parentItem]
-			@param {function} [opts.keyboardControls]
+			@param {function} [opts.keyboardControls] 
 			@param {boolean} [opts.focusActive] Sets whether focus is given on activation.
+			@param {selector|NodeList|String} [opts.returnFocusTo] Sets where to move focus to when the focussable object has been deactivated.
 	*/
+	
+	/* PROPERTIES?? */
 	
 	/**
 		@name glow.ui.FocusManager#event:itemClicked
