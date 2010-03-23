9818f2a753d6851401b95de61dc3d0e745034e7f
Michael Mathews <micmath@gmail.com>
2010-03-23 16:51:16 +0000

    Renamed.

--- src/ui/overlay/overlay.js
+++ src/ui/overlay/overlay.js
@@ -1,3 +1,6 @@
+Glow.provide(function(glow) {
+	var OverlayProto, undefined;
+	
 	/**
 		@name glow.ui.Overlay
 		@class
@@ -6,12 +9,10 @@
 		@param {selector|NodeList|String} content
 			the element that contains the contents of the overlay. If not in the document, you must append it to the document before calling show().
 
-		@param {object} opts
-			Zero or more of the following as properties of an object:
-			@param {Boolean} [opts.closeOnMaskClick=true] if true then listens for a click event on the mask and hides when it fires
-			@param {Number} [opts.zIndex="9991"] The z-index to set on the overlay
+		@param {object} [opts]
+			@param {number} [opts.zIndex="9991"] The z-index to set on the overlay.
 				If the overlay is modal, the zIndex of the mask will be set to one less than the value of this attribute.
-			@param {function|selector|NodeList} [opts.hideFilter] Exclude elements from hiding
+			@param {function|selector|NodeList} [opts.hideFilter] Exclude elements from hiding.
 				When provided this function is run for every element that may be hidden. This includes windowed
 				Flash movies if an intersection with the overlay is found.
 		@example
@@ -23,10 +24,43 @@
 				)
 			);
 			overlay.show();
+	 */
+	
+	function Overlay(content, opts) {
+		//call the base class's constructor
+		this.base = Overlay.base.call(this, 'overlay');
+		
+		this.init(opts);
+		this.attach(content, this.opts);
+	}
+	glow.util.extend(Overlay, glow.ui.Widget);
+	
+	OverlayProto = Overlay.prototype;
+	
+	OverlayProto.init = function(opts) {
+		this.base.init.call(this, opts);
+		
+		var defaults = {
+			zIndex: 9991
+		};
+		
+		this.opts = glow.util.apply(defaults, opts);
+		
+		/**
+			@name glow.ui.Overlay#visible
+			@description True if the overlay is showing.
+				This is a read-only property to check the state of the overlay.
+			@type boolean
 		*/
+		this.visible = false;
+	}
+	
+	OverlayProto.attach = function(content, opts) {
+		this.base.attach.call(this, content, opts);
 		
-		
-
+		this.container.css('z-index', this.opts.zIndex);
+	}
+	
 	/**
 		@name glow.ui.Overlay#event:show
 		@event
@@ -40,10 +74,11 @@
 		@param {glow.events.Event} event Event Object
 	*/
 	
+	
 	/**
 		@name glow.ui.Overlay#event:afterShow
 		@event
-		@description Fired when the overlay is visible to the user and any delay or 'show' animation is complete
+		@description Fired when the overlay is visible to the user and any delay or 'show' animation is complete.
 
 			This event is ideal to assign focus to a particular part of	the overlay.
 			If you want to change content of the overlay before it appears, see the 
@@ -55,7 +90,7 @@
 	/**
 		@name glow.ui.Overlay#event:hide
 		@event
-		@description Fired when the overlay is about to hide
+		@description Fired when the overlay is about to hide.
 
 			If you prevent the default action of this event (by returning false or 
 			calling event.preventDefault) the overlay will not hide.
@@ -66,66 +101,68 @@
 	/**
 		@name glow.ui.Overlay#event:afterHide
 		@event
-		@description Fired when the overlay has fully hidden, after any delay or hiding animation has completed
+		@description Fired when the overlay has fully hidden, after any delay or hiding animation has completed.
 		@param {glow.events.Event} event Event Object
 	*/
-		
-		
-		
-	/**
-		@name glow.ui.Overlay#visible
-		@description True if the overlay is showing.
-			This is a read-only property to check the state of the overlay.
-		@type Boolean
-	*/
-		
 	
 	/**
-		@name glow.ui.Overlay#setPosition
+		@name glow.ui.Overlay#setAnim
 		@function
-		@description Change or recalculate the position of the overlay
-			Call with parameters to
-			change the position of the overlay or call without parameters to recalculate
-			the position of the overlay. You may need to call this without parameters
-			if relative positions become invalid.
-
-		@param {Number|String} [x]
-			distance of overlay from the left of the viewport. If the unit is a percentage
-			then 0% is aligned to the left of the viewport, 100% is aligned to the right of viewport and 50% is centered.
-		@param {Number|String} [y]
-			distance of overlay from the top of the viewport. If the unit is a percentage
-			then 0% is aligned to the left of the viewport, 100% is aligned to the right of viewport and 50% is centered.
-
-		@returns this
+		@description Set the animation to use when showing and/or hiding the overlay.
+		@param {glow.Anim|null|undefined} showAnim The animation to use for showing this overlay.
+		Passing null will remove any previously set animation and indicate no opening animation should be used. Passing undefined will leave any previously set animation unchanged.
+		@param {glow.Anim|null|undefined} [hideAnim] The animation to use for closing this overlay.
+		Passing null will remove any previously set animation and indicate no closing animation should be used. Passing undefined will indicate the default animation should be used: the reverse of the show animation, if one was set.
 	*/
-	
+	OverlayProto.setAnim = function() { // (showAnim, hideAnim) could be word, like "fade"
+	}
 	
 	/**
 		@name glow.ui.Overlay#show
 		@function
-		@param {object} opts
-			Zero or more of the following as properties of an object:
-			@param {Number} [opts.delay=0] The delay before the overlay is shown
-				By default, the overlay will show immediately. Specify a number value of seconds to delay showing. The event "afterShow" will be called after any delay and animation.
-			@param {Object} [opts.anim] The animation to use when the overlay is shown
-				Bu default, no animation will be used. Provide an animation object to show the overlay with an effect. The event "afterShow" will be called after any animation and delay.
-		@description Displays the overlay after an optional delay period and animation
+		@param {object} [opts]
+			@param {number} [opts.delay=0] The delay before the overlay is shown.
+				By default, the overlay will show immediately. Specify a number value of microseconds to delay showing. The event "afterShow" will be called after any delay and animation.
+			@param {object} [opts.anim] The animation to use when the overlay is shown.
+				By default, no animation will be used. Provide an animation object to show the overlay with an effect. The event "afterShow" will be called after any animation and delay.
+		@description Displays the overlay after an optional delay period and animation.
 
 		@returns this
 	*/
+	OverlayProto.show = function() {
+		var e = new glow.events.Event();
+		this.fire('show', e);
+		
+		if (!e.defaultPrevented()) {
+			this.visible = true;
+			this.container.state.addClass('visible');
+		}
+	}
 	
 	/**
 		@name glow.ui.Overlay#hide
 		@function
-		@param {object} opts
-			Zero or more of the following as properties of an object:
-			@param {Number} [opts.delay=0] The delay before the overlay is hidden
-				By default, the overlay will hide immediately. Specify a number value of seconds to delay hiding.  The event "afterHide" will be called after any delay and animation.
-			@param {Object} [opts.anim] The animation to use when the overlay is shown
-				Bu default, no animation will be used. Provide an animation object to show the overlay with an effect. The event "afterHide" will be called after any animation and delay.
+		@param {object} [opts]
+			@param {number} [opts.delay=0] The delay before the overlay is hidden.
+				By default, the overlay will hide immediately. Specify a number value of microseconds to delay hiding.  The event "afterHide" will be called after any delay and animation.
+			@param {object} [opts.anim] The animation to use when the overlay is shown.
+				By default, no animation will be used. Provide an animation object to show the overlay with an effect. The event "afterHide" will be called after any animation and delay.
 		@description Hides the overlay after an optional delay period and animation
 
 		@returns this
 	*/
+	OverlayProto.hide = function() {
+		var e = new glow.events.Event();
+		this.fire('hide', e);
+		
+		if (!e.defaultPrevented()) {
+			this.visible = false;
+			this.container.state.removeClass('visible');
+		}
+	}
+
 	
-	
+	// export
+	glow.ui = glow.ui || {};
+	glow.ui.Overlay = Overlay;
+});
