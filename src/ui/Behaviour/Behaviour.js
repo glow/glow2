0455869bd68198c124be3bb0d3deec5668ed3dae
Jake Archibald <jake.archibald@bbc.co.uk>
2010-03-23 13:44:06 +0000

    Behaviour: Adding small proposal

--- src/ui/Behaviour/Behaviour.js
+++ src/ui/Behaviour/Behaviour.js
@@ -1,3 +1,32 @@
 Glow.provide(function(glow) {
 	var BehaviourProto, undefined;
+	
+	/**
+		@name glow.ui.Behaviour
+		@class
+		@extends glow.events.Target
+		@description Abstract behaviour class.
+		@param {string} name The name of this widget.
+		This is added to class names in the generated DOM nodes that wrap the widget interface.
+		
+	*/
+	function Behaviour() {}
+	
+	/**
+		@name glow.ui.Behaviour#disable
+		@method
+		@description Sets the disabled property of this behaviour to true and fires the disable event
+		
+		@param {boolean} [state=true] 
+		@see glow.ui.Widget#enable
+	*/
+	
+	/**
+		@name glow.ui.Behaviour#destroy
+		@function
+		@description Removes the behaviour & event listeners
+	*/
+	
+	// EXPORT
+	glow.ui.Behaviour = Behaviour;
 });
