caf7f615021098d05fe8bf1186c9c3e8d8900075
Frances Berriman <frances.berriman@bbc.co.uk>
2010-04-15 16:47:37 +0100

    messing with tests for net

--- test/unit/glow/net/net.js
+++ test/unit/glow/net/net.js
@@ -10,262 +10,248 @@ module('glow.net');
 		stop();	
 	
 		var getRequest = glow.net.get("xhr/basictext.txt").on("load", 
-				function(response){
-					ok(true, "correct callback used");
-					equal(response.status, 200, "Status code");
-					equal(response.nativeResponse.status, 200, "Native response found");
-					ok(response.statusText(), "Status returned: " + response.statusText());
-					equal(response.header("Content-Type"), "text/plain", "Content-Type header");
-					start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
+			function(response){
+				ok(true, 'correct callback used');
+				equal(response.status, 200, 'Status code');
+				equal(response.nativeResponse.status, 200, 'Native response found');
+				ok(response.statusText(), 'Status returned: ' + response.statusText());
+				equal(response.header('Content-Type'), 'text/plain', 'Content-Type header');
+				start();
+			})
+		.on('error',
+			function(response){
+				ok(false, 'correct callback used');
+				start();
+			});
 		
-		equal(typeof getRequest.abort, "function", "Return object has abort method");
-		equal(typeof getRequest.on, "function", "Return object has on method");
-		equal(typeof getRequest.destroy, "function", "Return object has destroy method");
-	});
-	
-	
-	
-	/*test("glow.net.get async header setting", function() {
-		expect(5);
+		equal(typeof getRequest.abort, 'function', 'Return object has abort method');
+		equal(typeof getRequest.on, 'function', 'Return object has on method');
+		equal(typeof getRequest.destroy, 'function', 'Return object has destroy method');
+		
 		stop(5000);
-		
-		
-		var request = glow.net.get("xhr/requestheaderdump.php",
-					 {headers: {
-							"Custom-Header": "thisisatest",
-							"Content-Type": "image/png"
-						}}
-					 ).on("load", 
-				function(response){
-					if ( response.text().slice(0, 2) == '<?' ) {
-					start();
-					skip("This test requires a web server running PHP5");
-					return;
-				}
-						ok(true, "correct callback used");
-						ok(/^REQUEST_METHOD: GET/m.test(response.text()), "Using get method");
-						ok(/^HTTP_CUSTOM_HEADER: thisisatest/m.test(response.text()), "Custom Header Sent");
-						ok(/^HTTP_X_REQUESTED_WITH: XMLHttpRequest/m.test(response.text()), "X-Requested-With default set");
-						ok(/^CONTENT_TYPE: image\/png/m.test(response.text()), "Content-type Changed");
-						start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-		
-		
 	});
-	*/
+	
+	
+	
+	test('glow.net.get async header setting', function() {
+		expect(5);
+		stop();
+		
+		
+		var request = glow.net.get('xhr/requestheaderdump.php',
+			{headers: {
+				'Custom-Header': 'thisisatest',
+				'Content-Type': 'image/png'
+			}})
+		.on('load', 
+			function(response){
+				ok(true, 'correct callback used');
+				ok(/^REQUEST_METHOD: GET/m.test(response.text()), 'Using get method');
+				ok(/^HTTP_CUSTOM_HEADER: thisisatest/m.test(response.text()), "Custom Header Sent");
+				ok(/^HTTP_X_REQUESTED_WITH: XMLHttpRequest/m.test(response.text()), "X-Requested-With default set");
+				ok(/^CONTENT_TYPE: image\/png/m.test(response.text()), "Content-type Changed");
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});
+		stop(5000);	
+	});
+
 	
 	test("glow.net.get async xml", function() {
-	expect(3);
-	stop(5000);
-	
-			var getXml = glow.net.get("xhr/xml.xml").on("load", 
-				function(response){
-					ok(true, "correct callback used");
-					var xml = response.xml();
-					ok(xml, "xml returned");
-					equal(xml.getElementsByTagName("foo").length, 3, "3 elements of foo");
-					start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-			
-			
-	
+		expect(3);
+		stop();
+		
+		var getXml = glow.net.get("xhr/xml.xml").on("load", 
+			function(response){
+				ok(true, "correct callback used");
+				var xml = response.xml();
+				ok(xml, "xml returned");
+				equal(xml.getElementsByTagName("foo").length, 3, "3 elements of foo");
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});			
+		stop(5000);
 	});
 	
 	
 	test("glow.net.get force xml", function() {
 		expect(5);
-		stop(5000);
+		stop();
 		
 		var getXml = glow.net.get("xhr/xml.txt", {
-			forceXml: true}).on("load", 
-				function(response){
-					ok(true, "correct callback used");
-					var xml = response.xml();
-					ok(xml, "xml returned");
-					equal(xml.getElementsByTagName("hello").length, 1, "1 element of hello");
-					equal(xml.getElementsByTagName("world").length, 1, "1 element of world");
-					equal(xml.getElementsByTagName("world")[0].childNodes[0].nodeValue, 'Yey for XML', "Got text node value");
-					start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-		
-		
-		
+			forceXml: true})
+		.on("load", 
+			function(response){
+				ok(true, "correct callback used");
+				var xml = response.xml();
+				ok(xml, "xml returned");
+				equal(xml.getElementsByTagName("hello").length, 1, "1 element of hello");
+				equal(xml.getElementsByTagName("world").length, 1, "1 element of world");
+				equal(xml.getElementsByTagName("world")[0].childNodes[0].nodeValue, 'Yey for XML', "Got text node value");
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});	
+		stop(5000);
 	});
 
 	// need decodeJson method
 	test("glow.net.get async json", function() {
 		expect(4);
-		stop(5000);
+		stop();
 		
 		var getRequest = glow.net.get("xhr/json.txt", 
-		{some:"postData", blah:["something", "somethingElse"]}).on("load", 
-				function(response){
-					ok(true, "correct callback used");
-					var json = response.json();
-					ok(json, "json returned");
-					equal(json.hello, "world", "Returned correct value for 'hello'");
-					equal(json.something, 3, "Returned correct value for 'something'");
-					start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-		
-		
+			{some:"postData", blah:["something", "somethingElse"]})
+		.on("load", 
+			function(response){
+				ok(true, "correct callback used");
+				var json = response.json();
+				ok(json, "json returned");
+				equal(json.hello, "world", "Returned correct value for 'hello'");
+				equal(json.something, 3, "Returned correct value for 'something'");
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});
+		stop(5000);
 	});
+	
 	// below happens on all current version of IE 6, 7, 8
 	if(!glow.env.ie){
-	test("glow.net.abort", function() {
-		expect(2);
-		
-		
-		
-		stop(5000);
-		var aborted = true;
-		var abortableRequest = glow.net.get("xhr/large.txt").on("load", 
+		test("glow.net.abort", function() {
+			expect(2);		
+			
+			stop();
+			var aborted = true;
+			var abortableRequest = glow.net.get("xhr/large.txt").on("load", 
 				function(response){
 					aborted = false;
-				}).on("error",
+				})
+			.on("error",
 				function(response){
 					aborted = false;
-				}).on("abort",
+				})
+			.on("abort",
 				function(response){
 					ok(true, "Abort event fired");
+					start();
 				});
-		abortableRequest.abort();
-		window.setTimeout(function() {
-			ok(aborted, "Request aborted");
-			start();
-		}, 1000);
-		
-		
-	});
+			
+			abortableRequest.abort();
+	
+			ok(aborted, "Request aborted");	
+			stop(5000);
+		});
 	}
 	
 	
 	test("glow.net.post async string", function() {
-	expect(3);
-	stop(5000);
-	var postRequest = glow.net.post("xhr/requestheaderdump.php",
-					"some=postData&blah=hurrah").on("load",
-				  function(response){
-					if ( response.text().slice(0, 2) == '<?' ) {
-						start();
-						skip("This test requires a web server running PHP5");
-						return;
-					}
-					ok(true, "correct callback used");
-					equal( (/^REQUEST_METHOD: (\w+)/m.exec(response.text()) || [,,])[1], "POST", "Using post method" );
-					equal( (/^CONTENT_LENGTH: (\d+)/m.exec(response.text()) || [,,])[1], "25",   "Correct content length" );
-					start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-	
-	
-});
+		expect(3);
+		stop();
+		var postRequest = glow.net.post("xhr/requestheaderdump.php",
+					"some=postData&blah=hurrah")
+		.on("load",
+			function(response){					
+				ok(true, "correct callback used");
+				equal( (/^REQUEST_METHOD: (\w+)/m.exec(response.text()) || [,,])[1], "POST", "Using post method" );
+				equal( (/^CONTENT_LENGTH: (\d+)/m.exec(response.text()) || [,,])[1], "25",   "Correct content length" );
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});
+		stop(5000);
+	});
 	
 	test("glow.net.post aync json", function() {
-	expect(3);
-	stop(5000);
-	
-	var postRequest = glow.net.post("xhr/requestheaderdump.php",
-								{some:"postData", blah:["something", "somethingElse"]}).on("load",
-					function(response){
-						if ( response.text().slice(0, 2) == '<?' ) {
-						start();
-						skip("This test requires a web server running PHP5");
-						return;
-					}
-						ok(true, "correct callback used");
-						equal( (/^REQUEST_METHOD: (\w+)/m.exec(response.text()) || [,,])[1], "POST", "Using post method" );
-						equal( (/^CONTENT_LENGTH: (\d+)/m.exec(response.text()) || [,,])[1], "47",   "Correct content length" );
-						start();
-				}).on("error",
-				function(response){
-					ok(false, "correct callback used");
-					start();
-				});
-	
-	
+		expect(3);
+		stop();
+		
+		var postRequest = glow.net.post("xhr/requestheaderdump.php",
+					{some:"postData", blah:["something", "somethingElse"]}).on("load",
+			function(response){				
+				ok(true, "correct callback used");
+				equal( (/^REQUEST_METHOD: (\w+)/m.exec(response.text()) || [,,])[1], "POST", "Using post method" );
+				equal( (/^CONTENT_LENGTH: (\d+)/m.exec(response.text()) || [,,])[1], "47",   "Correct content length" );
+				start();
+			})
+		.on("error",
+			function(response){
+				ok(false, "correct callback used");
+				start();
+			});
+		stop(5000);
 	});
 
 
-//timeouts
-test("glow.net.get timeout cancelling", function() {
-	expect(2);
-	stop(5000);
+
+	test("glow.net.get timeout cancelling", function() {
+		expect(2);
+		stop();
+		
+		var noError = true;
+		
+		var getRequest = glow.net.get("xhr/basictext.txt",
+				{timeout: 2}).on("load",
+			function(response){
+				ok(true, "load called");
+				start();
+			})
+		.on("error",
+			function(response){
+				noError = false;
+			});
+		
 	
-	var noError = true;
-	
-	var getRequest = glow.net.get("xhr/basictext.txt",
-					{timeout: 2}).on("load",
-				function(response){
-					ok(true, "load called");
-							
-				}).on("error",
-				function(response){
-					noError = false;
+		ok(noError, "error (timeout) not called");		
+		stop(5000);
+	});
+
+
+	test("glow.net.getJsonp general", function() {
+		expect(3);
+		stop();
+		var timeoutCancelled = true;
+		
+		var jsonpRequest = glow.net.getJsonp("xhr/jsoncallback.js?callback={callback}",
+						   {timeout: 2}).on('load',
+			function(data) {
+				ok(true, "Callback called");
+				equal(data.hello, "world", "Data returned");
+				start();
+			}).on('error',
+				function() {
+					timeoutCancelled = false;
+					
 				});
 	
 	
+			ok(timeoutCancelled, "error (timeout) not called")
+			
+	
+		stop(5000);
 		
-	window.setTimeout(function () {
-		ok(noError, "error (timeout) not called")
-		start();
-	}, 3000);
-});
-
-/**** HERE ****/
-test("glow.net.getJsonp general", function() {
-	expect(2);
-	stop(5000);
-	var timeoutCancelled = true;
-	
-	var jsonpRequest = glow.net.getJsonp("xhr/jsoncallback.js?callback={callback}",
-					   {timeout: 2}).on('load',
-		function(data) {
-			ok(true, "Callback called");
-			equal(data.hello, "world", "Data returned");
-			start();
-		}).on('error',
-			function() {
-				timeoutCancelled = false;
-				
-			});
-
-	window.setTimeout(function () {
-		ok(timeoutCancelled, "error (timeout) not called")
-		
-		start();
-	}, 3000);
-	
-	
-});
+	});
 
 test("glow.net.getJsonp timeout and charset", function() {
 	expect(3);
-	stop(5000);
+	stop();
 	
 	var onLoadCalled = false;
 	
@@ -275,7 +261,6 @@ test("glow.net.getJsonp timeout and charset", function() {
 						charset: "utf-8"}).on('load', 
 		function(data) {
 			onLoadCalled = true;
-			start();
 		}).on('error',
 		function() {
 			ok(!onLoadCalled, "load not called");
@@ -284,6 +269,7 @@ test("glow.net.getJsonp timeout and charset", function() {
 		});
 
 	equals(glow(document.body.lastChild).attr("charset"), "utf-8", "Charset set");
+	stop(5000);
 });
 
 test("glow.net.getJsonp aborting", function() {	
@@ -301,7 +287,8 @@ test("glow.net.getJsonp aborting", function() {
 			onErrorCalled = true;			
 		}).on('abort',
 		function() {
-			onAbortCalled = true;			
+			onAbortCalled = true;
+			start();
 		});
 		
 	
@@ -309,113 +296,98 @@ test("glow.net.getJsonp aborting", function() {
 		
 	jsonpRequest.abort();
 	
-	window.setTimeout(function () {
-		//if (!request.completed) {
-			expect(3);
-			ok(!onLoadCalled, "load not called");
-			ok(!onErrorCalled, "error (timeout) not called");
-			ok(onAbortCalled, "abort called");
-			start();
-	//	}
-	//	else{
-	//		expect(1);
-	//		ok(request.completed, "The request completed to quickly to abort it");
-	//		start();
-	//	}
-	}, 8000);
+	
+	expect(3);
+	ok(!onLoadCalled, "load not called");
+	ok(!onErrorCalled, "error (timeout) not called");
+	ok(onAbortCalled, "abort called");
+			
+	stop(5000);
 });
 
 
 
 test("glow.net.getResources single CSS", function() {
 	expect(5);
-	stop();
+	stop(5000);
 	var timeoutCancelled = true;
 	
-	var cssRequest = glow.net.getResources("http://www.bbc.co.uk/glow/styles/default.css",
-					   {timeout: 2}).on('progress',
+	var cssRequest = glow.net.getResources("http://www.bbc.co.uk/glow/styles/default.css").on('progress',
 		function(response){
-			ok(true, "Progress fired");
+			ok(true, "Progress fired for single CSS file");
 			equal( response, "http://www.bbc.co.uk/glow/styles/default.css", "Got uri of the item that just completed (progress)" );
 			}).on('load',
 		function(response) {
 			ok(true, "Load fired");
-			console.log(response.element());
-			equal( response.element().length, 1, "Load returned 1 item" );
+			equal( response.completed, 1, "1 item completed" );
+			start();
 		}).on('error',
 			function() {
-				console.log('error event');
 				timeoutCancelled = false;
 			});
 
+	ok(timeoutCancelled, "error (timeout) not called");
 	
-	window.setTimeout(function () {
-		ok(timeoutCancelled, "error (timeout) not called");
-		
-		start();
-	}, 10000);
 	
 	
 });
 
 test("glow.net.getResources single image", function() {
 	expect(5);
-	stop();
+	stop(5000);
 	var timeoutCancelled = true;
 	
-	var image = glow.net.getResources("http://www.bbc.co.uk/glow/styles/images/banner.png",
-					   {timeout: 2}).on('progress',
+	var image = glow.net.getResources("http://www.bbc.co.uk/glow/styles/images/banner.png")
+	.on('progress',
 		function(response){
-			ok(true, "Progress fired (this should appear 1 time)");
+			ok(true, "Progress fired for single image (this should appear 1 time)");
 			equal( response, "http://www.bbc.co.uk/glow/styles/images/banner.png", "Got uri of the item that just completed (progress)" );
 			
 			}).on('load',
 		function(response) {
 			ok(true, "Load fired");
 			equal( response.completed, 1, "1 item completed" );
+			start();
 		}).on('error',
 			function() {
 				timeoutCancelled = false;
+				
 			});
 	
-	
-	//console.log(request.element());
-	
-	window.setTimeout(function () {
-		ok(timeoutCancelled, "error (timeout) not called")
-		
-		start();
-	}, 10000);
-	
-	
+	ok(timeoutCancelled, "error (timeout) not called")
+
+
 });
 
 test("glow.net.getResources multiple images", function() {
 	expect(5);
-	stop();
+
+	stop(5000);
 	var timeoutCancelled = true;
 	
-	var images = glow.net.getResources(["http://www.bbc.co.uk/glow/styles/images/banner.png", "http://www.bbc.co.uk/includes/blq/resources/gvl/r57/img/header_blocks.gif"],
-					   {timeout: 2}).on('progress',
+	var images = glow.net.getResources(["xhr/resources/glow_g.png", "xhr/resources/bunny.jpg"])
+	.on('progress',
 		function(response){
 			ok(true, "Progress fired (this should appear 2 times)");
-			}).on('load',
+			
+			})
+	.on('load',
 		function(response) {
 			ok(true, "Load fired");
 			equal( response.completed, 2, "2 items completed" );
+			start()
 		}).on('error',
 			function() {
 				timeoutCancelled = false;
+			
 			});
 	
 	
-	//console.log(request.element());
 	
-	window.setTimeout(function () {
 		ok(timeoutCancelled, "error (timeout) not called")
 		
-		start();
-	}, 10000);
+	
+
 	
 	
 });
@@ -423,29 +395,30 @@ test("glow.net.getResources multiple images", function() {
 
 test("glow.net.getResources mixed images and css", function() {
 	expect(6);
-	stop();
+
+	stop(5000);
 	var timeoutCancelled = true;
 	
 	var imagesAndCss = glow.net.getResources(["http://www.bbc.co.uk/glow/styles/images/banner.png", "http://www.bbc.co.uk/includes/blq/resources/gvl/r57/img/header_blocks.gif", "http://www.bbc.co.uk/glow/styles/default.css"],
 					   {timeout: 2}).on('progress',
-		function(response){
+	function(response){
 			ok(true, "Progress fired (this should appear 3 times)");
-			}).on('load',
+			})
+	.on('load',
 		function(response) {
 			ok(true, "Load fired");
 			equal( response.completed, 3, "3 items completed" );
-		}).on('error',
+			start();
+		})
+	.on('error',
 			function() {
 				timeoutCancelled = false;
-			});
+				start();
+		});
 	
-
-	
-	window.setTimeout(function () {
-		ok(timeoutCancelled, "error (timeout) not called")
+	ok(timeoutCancelled, "error (timeout) not called")
 		
-		start();
-	}, 10000);
+
 	
 	
 });
@@ -454,7 +427,8 @@ test("glow.net.put json", function() {
 	expect(2);
 	stop();
 	var putRequest = glow.net.put("xhr/put.php",
-		{some:"putData", blah:["something", "somethingElse"]}).on('load',
+		{some:"putData", blah:["something", "somethingElse"]})
+	.on('load',
 			function(response) {				
 				ok(true, "correct callback used");
 				equal( response.text(), "PUT: putData", "Using put method" );
@@ -464,7 +438,7 @@ test("glow.net.put json", function() {
 				ok(false, "correct callback used");
 				start();
 			});
-
+	stop(5000);
 });
 
 
@@ -481,7 +455,7 @@ test("glow.net.del", function() {
 				ok(false, "correct callback used");
 				start();
 			});
-		
+	stop(5000);	
 	
 });
 
@@ -495,6 +469,6 @@ test("glow.net.crossDomainRequest", function () {
             equal(response.text(), 'test response', 'get xDomainResponse');
             start();
 	   });
-
+	stop(5000);
 
 });
